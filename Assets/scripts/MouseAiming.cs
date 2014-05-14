2283db32108718e76131eb035742313d11bba151
Juha Kleemola <juha.kleemola@cou.fi>
2014-05-14 12:28:46 +0300

    Commented code in MouseAiming.cs

--- Assets/scripts/MouseAiming.cs
+++ Assets/scripts/MouseAiming.cs
@@ -5,8 +5,8 @@ public class MouseAiming : MonoBehaviour {
 
 	//Objects and stuff
 
-	public GameObject HeadObject; //Used for head angle
-	public GameObject BodyObject; //Used for the body rotation
+	public GameObject HeadObject; //Used in head angle and camera parenting.
+	public GameObject BodyObject; //Used in body rotation
 
 	private Transform HeadObjectLocationXYZ; //XYZ location of HeadObject
 
@@ -40,14 +40,14 @@ public class MouseAiming : MonoBehaviour {
 	
 	float RotationY = 0F;
 	float RotationX = 0F;
-
+	/* These are not used anywhere yet. They might be used later on when we implement a separate clamp for the camera.
 	float CameraRotationX = 0F;
 	float CameraRotationY = 0F;
 	float CameraRotationZ = 0F;
-	
+	*/
 	void Start ()
 	{
-		Camera.main.transform.position = new Vector3(0, 0, 0);
+		Camera.main.transform.position = new Vector3(0, 0, 0); //Moves camera to 0,0,0
 
 		HeadObjectLocationXYZ = HeadObject.transform;
 
@@ -55,11 +55,11 @@ public class MouseAiming : MonoBehaviour {
 		HeadObjectLocationY = HeadObjectLocationXYZ.transform.position.y;
 		HeadObjectLocationZ = HeadObjectLocationXYZ.transform.position.z;
 
-		Camera.main.transform.position = new Vector3(HeadObjectLocationX+CameraDistanceX, HeadObjectLocationY+CameraDistanceY,HeadObjectLocationZ+CameraDistanceZ);
+		Camera.main.transform.position = new Vector3(HeadObjectLocationX+CameraDistanceX, HeadObjectLocationY+CameraDistanceY,HeadObjectLocationZ+CameraDistanceZ); //Moves the camera to the heads location and adds some offset to it to make it a 3rd person camera.
 
-		Camera.main.transform.rotation = Quaternion.Euler(CameraAngleX, CameraAngleY, CameraAngleZ);
+		Camera.main.transform.rotation = Quaternion.Euler(CameraAngleX, CameraAngleY, CameraAngleZ); //Controls the camera angle
 
-		Camera.main.transform.parent = HeadObject.transform;
+		Camera.main.transform.parent = HeadObject.transform;	//Parents the main camera to the head.
 
 		//Screen.lockCursor= true;
 
@@ -71,20 +71,20 @@ public class MouseAiming : MonoBehaviour {
 		RotationY += Input.GetAxis("Mouse Y")*SensitivityY*Time.deltaTime;
 
 
-		if (InvertedY == false) //Normal mouse
+		if (InvertedY == false) //Normal mouse | If you edit this, don't forget to copy and paste the changes to inverted mouse.
 		{
 			
-			RotationY = Mathf.Clamp(RotationY, HeadObjectMinimumY, HeadObjectMaximumY);
+			RotationY = Mathf.Clamp(RotationY, HeadObjectMinimumY, HeadObjectMaximumY); //Clamps the angle. ie. head will not rotate 360 along its Y-axis
 			
 			BodyObject.transform.rotation = Quaternion.Euler(0,RotationX,0); //rotates the body
-			HeadObject.transform.rotation = Quaternion.Euler(0,RotationX-90,RotationY-90); //rotates the head on Y axis with a clamp. Also turns head on X axis so it turns with the body
+			HeadObject.transform.rotation = Quaternion.Euler(0,RotationX-90,RotationY-90); //rotates the heads Y axis. Also turns head on X axis so it turns with the body. | The axis on the unity default character are a bit wonky at the moment.
 			
 		} 
 		
-		else //Inverted mouse
+		else //Inverted mouse | Some of the variables work bit differently here than on the normal mouse and you may need to use negative values for some. For example the Mathf.Clamp.
 		{
 			
-			RotationY = Mathf.Clamp(RotationY, -HeadObjectMaximumY, -HeadObjectMinimumY);
+			RotationY = Mathf.Clamp(RotationY, -HeadObjectMaximumY, -HeadObjectMinimumY); //Clamps the angle. ie. head will not rotate 360 along its Y-axis
 			
 			BodyObject.transform.rotation = Quaternion.Euler(0,RotationX,0); //rotates the body
 			HeadObject.transform.rotation = Quaternion.Euler(0,RotationX-90,-RotationY-90); //rotates the head on Y axis with a clamp. Also turns head on X axis so it turns with the body
