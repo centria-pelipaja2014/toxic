a3a960be93cacf4bc14ea762e48dfa27d007b807
Niko kaukonen <niko.kaukonen@cou.fi>
2014-05-21 14:57:10 +0300

    Added the new default cursor icon and light script for main menu.

---
+++ Assets/scripts/MouseLight.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLight : MonoBehaviour {
+	
+	//public Vector3 mousePosition;
+	public GameObject mouseLight;
+	// Use this for initialization
+	private Vector3 WorldPos;
+
+	void Start () {
+
+
+
+		mouseLight = GameObject.Find ( "cursorLight" );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		WorldPos = Camera.main.ScreenToWorldPoint ( new Vector3( Input.mousePosition.x, Input.mousePosition.y, 8.5f ) );
+		//Vector3 mousePosition = camera.ScreenToWorldPoint (Input.mousePosition);
+		//mouseLight.transform.Translate( WorldPos * Time.deltaTime);
+
+		mouseLight.transform.position = WorldPos;
+
+	}
+}
